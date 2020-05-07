f642d66c0328e8097f848b07bf0ffa76f3a741b9
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2020-05-08 00:03:08 +0200

    Fix small bug

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -17,14 +17,13 @@ urlpatterns = [
 	path('managetask/<int:id>', views.managetask, name='managetask'),
 	path('dashboard', views.dashboard, name='dashboard'),
 
-	path('calendar', views.calendar, name='calendar'),
 	path('members/',views.projects_members, name='membersproject'),
 	path('tasks/',views.list_tasks,name='listtasks'),
 	path('finished/',views.finished_tasks,name='finishedtasks'),
 	path('distinct/<int:ide>', views.distinct_tasks, name='distincttasks'),
 	path('activities/<int:ide>', views.activities, name='activities'),
 
-  path('graphs', views_graph.graphs, name='graphs'),
+	path('graphs', views_graph.graphs, name='graphs'),
 	path('gantt', views_graph.gantt, name='gantt'),
 	path('activitydiag', views_graph.activitydiag, name='activitydiag'),
 	path('burndown', views_graph.burndown, name='burndown'),
