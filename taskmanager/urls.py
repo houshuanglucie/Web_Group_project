0dac1d72e9dff55792b3ed734a38115863615854
Mikan <clementine.allanic@gmail.com>
2020-05-10 17:59:11 +0200

    Merge remote-tracking branch 'origin/master'
    Reformatage du fichier urls

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -4,43 +4,37 @@ from . import views_graph
 from . import views_export
 
 urlpatterns = [
-	path('', views.redirect_home, name = 'redirect_home'),
-	path('home/', views.home, name = 'home'),
-	# path('connect/', views.connect, name = 'connect'),
-	path('connect/', views.LoginPage.as_view(), name = 'connect'),
-	path('disconnect/', views.disconnect, name = 'disconnect'),
-	path('projects/', views.projects, name = 'projects'),
-	path('projects/<int:id>', views.focus_project, name = 'focus_project'),
-	path('task/<int:id>', views.focus_task, name = 'focus_task'),
-	path('newproject', views.newproject, name = 'newproject'),
-	path('manageproject/<int:id>', views.manageproject, name = 'manageproject'),
-	path('newtask/<int:id_project>', views.newtask, name='newtask'),
-	path('managetask/<int:id>', views.managetask, name='managetask'),
+    path('', views.redirect_home, name='redirect_home'),
+    path('home/', views.home, name='home'),
+    # path('connect/', views.connect, name = 'connect'),
+    path('connect/', views.LoginPage.as_view(), name='connect'),
+    path('disconnect/', views.disconnect, name='disconnect'),
+    path('projects/', views.projects, name='projects'),
+    path('projects/<int:id>', views.focus_project, name='focus_project'),
+    path('task/<int:id>', views.focus_task, name='focus_task'),
+    path('newproject', views.newproject, name='newproject'),
+    path('manageproject/<int:id>', views.manageproject, name='manageproject'),
+    path('newtask/<int:id_project>', views.newtask, name='newtask'),
+    path('managetask/<int:id>', views.managetask, name='managetask'),
 
-	path('members/',views.projects_members, name='membersproject'),
-	path('tasks/',views.list_tasks,name='listtasks'),
-	path('finished/',views.finished_tasks,name='finishedtasks'),
-	path('distinct/<int:ide>', views.distinct_tasks, name='distincttasks'),
-	path('activities/<int:ide>', views.activities, name='activities'),
+    path('members/', views.projects_members, name='membersproject'),
+    path('tasks/', views.list_tasks, name='listtasks'),
+    path('finished/', views.finished_tasks, name='finishedtasks'),
+    path('distinct/<int:ide>', views.distinct_tasks, name='distincttasks'),
+    path('activities/<int:ide>', views.activities, name='activities'),
 
+    path('dashboard', views_graph.dashboard, name='dashboard'),
+    path('graphs', views_graph.graphs, name='graphs'),
 
+    path('gantt', views_graph.gantt, name='gantt'),
 
-	path('dashboard', views_graph.dashboard, name='dashboard'),
-	path('graphs', views_graph.graphs, name='graphs'),
+    path('burndown', views_graph.burndown, name='burndown'),
+    path('radartask', views_graph.radartask, name='radartask'),
+    path('radaractivity', views_graph.radaractivity, name='radaractivity'),
+    path('manageapp', views_graph.manageapp, name='manageapp'),
 
-	path('gantt', views_graph.gantt, name='gantt'),
+    path('taskfilter/', views.task_filter, name='task_filter'),
 
-
-	path('burndown', views_graph.burndown, name='burndown'),
-	path('radartask', views_graph.radartask, name='radartask'),
-	path('radaractivity', views_graph.radaractivity, name='radaractivity'),
-	path('manageapp', views_graph.manageapp, name='manageapp'),
-
-
-
-	path('taskfilter/', views.task_filter, name='task_filter'),
-
-  
-  path('export/xml', views_export.export_xml, name='export_xml'),
-	path('export/json', views_export.export_json, name='export_json'),
+    path('export/xml', views_export.export_xml, name='export_xml'),
+    path('export/json', views_export.export_json, name='export_json'),
 ]
