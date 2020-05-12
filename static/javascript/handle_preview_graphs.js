74768f84aa8350b07cb45ebb7b8afec61e1469c6
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2020-05-12 11:17:45 +0200

    Finish preview graphs

--- static/javascript/handle_preview_graphs.js
+++ static/javascript/handle_preview_graphs.js
@@ -10,34 +10,37 @@ function handle_preview_graphs(){
    // CUSTOM DASHBOARD
    $('#dashboard-nav_item').hover(function() {
       $('#name_graph').text("Tableau de bord");
+      $('#description_graph').text("Un tableau de bord customisable avec les représentations que vous choisissez");
+      $('#image_graph').attr("src", "../../static/pictures/graphs_preview/dashboard.png");
    });
 
    // GANTT
    $('#gantt-nav_item').hover(function() {
       $('#name_graph').text("Diagramme de Gantt");
+      $('#description_graph').text("Une vue temporelle de vos projets et de vos activités");
+      $('#image_graph').attr("src", "../../static/pictures/graphs_preview/gantt.png");
    });
-
-
-   // DIAGRAMME D'ACTIVITES
-   $('#activitydiag-nav_item').hover(function() {
-      $('#name_graph').text("Diagramme d'activités");
-   });
-
 
    // BURNDOWN
    $('#burndown-nav_item').hover(function() {
       $('#name_graph').text("Burndown chart");
+      $('#description_graph').text("Une représentation de votre avancée dans chaque tâche et projet");
+      $('#image_graph').attr("src", "../../static/pictures/graphs_preview/burndown.png");
    });
 
 
    // RADAR TASK
    $('#radartask-nav_item').hover(function() {
       $('#name_graph').text("Radar des tâches");
+      $('#description_graph').text("Pour voir qui va beaucoup travailler dans vos projets");
+      $('#image_graph').attr("src", "../../static/pictures/graphs_preview/tasks.png");
    });
 
 
    // RADAR ACTIVTY
    $('#radaractivity-nav_item').hover(function() {
       $('#name_graph').text("Radar d'activités");
+      $('#description_graph').text("Pour voir si VOUS travaillez beaucoup dans vos projets");
+      $('#image_graph').attr("src", "../../static/pictures/graphs_preview/activity.png");
    });
 }
